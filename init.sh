f1fa44db6f9f726451a6fdabaacbe55b0c079dc0
root <root@ip-172-31-5-104.ap-northeast-1.compute.internal>
2018-06-11 08:37:14 +0000

    .

---
+++ init.sh
@@ -0,0 +1,4 @@
+HOSTNAME=`hostname`
+mkdir -p /mnt/weka/$HOSTNAME
+cd /mnt/weka/$HOSTNAME
+git clone https://github.com/marcus-sds/fio_scripts.git
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 HOSTNAME=`hostname`
+cd /mnt/weka/$HOSTNAME/fio_scripts
+
 rm -f *.job
 rm -f *.out
-mkdir -p /mnt/weka/$HOSTNAME
+
 ./fio.sh -w /mnt/weka/$HOSTNAME -t $1 -s 10 -m 1000 -f
-#./fioparse.sh -v *.out
