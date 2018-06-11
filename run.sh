dc86fe63db2fe07f53e539487151e2f56f8a264b
root <root@ip-172-31-5-104.ap-northeast-1.compute.internal>
2018-06-11 08:21:15 +0000

    .

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 HOSTNAME=`hostname`
-rm -f fio_scripts/*.job
-rm -f fio_scripts/*.out
+rm -f *.job
+rm -f *.out
 mkdir -p /mnt/weka/$HOSTNAME
-fio_scripts/fio.sh -w /mnt/weka/$HOSTNAME -t $1 -s 10 -m 1000 -f
-fio_scripts/fioparse.sh -v fio_scripts/*.out
+./fio.sh -w /mnt/weka/$HOSTNAME -t $1 -s 10 -m 1000 -f
+./fioparse.sh -v *.out
